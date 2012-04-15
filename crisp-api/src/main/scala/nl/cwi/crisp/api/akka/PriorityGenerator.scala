c69e63a0dbac699a30290cd6d217f5ed46932047
Behrooz Nobakht <behrooz.nobakht@gmail.com>
2012-04-15 11:32:49 +0200

    Priority Generator
    Calculating the priority of a received message with a safer approach.

--- crisp-api/src/main/scala/nl/cwi/crisp/api/akka/PriorityGenerator.scala
+++ crisp-api/src/main/scala/nl/cwi/crisp/api/akka/PriorityGenerator.scala
@@ -3,9 +3,15 @@ package nl.cwi.crisp.api.akka
 class PriorityGenerator extends akka.dispatch.PriorityGenerator {
   
   override def gen(message: Any): Int = {
-    var msg: PriorityMessage = message.asInstanceOf[PriorityMessage]
-    val p = msg.priority.getOrElse(0)
-    p
+    try {
+    	var msg: PriorityMessage = message.asInstanceOf[PriorityMessage]
+	    val p = msg.priority.getOrElse(0)
+	    println("priority msg [" + message + "] with p: " + p)
+    	p
+    } catch {
+    	case e: Exception => println("Cannot cast " + message + " to a priority message")
+    	0
+    }
   } 
   
 }
